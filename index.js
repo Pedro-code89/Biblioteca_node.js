4aa73958d0a65ba1228cd5c28b68b6d88dfcf741
Pedro Henrique Carvalho Santos <00001109816728sp@aluno.educacao.sp.gov.br>
2026-08-10 09:04:11 -0300

    Feat: Adicionando os arquivos

---
+++ index.js
@@ -0,0 +1,33 @@
+/* IMPORTANDO DEPENDÊNCIAS */
+
+
+const express = require('express');
+
+
+// IMPORTANDO AS ROTAS
+const routes = require('.src/routes/web')
+
+
+/* CONFIGURAÇÕES DO SERVIDOR */
+
+
+const app = express() /* LIGANDO O EXPRESS */
+const port = 3000 /* DEFININDO A PORTA */
+
+app.set('view engine', 'ejs')
+
+//Definindo ejs como motor de template padrão
+app.set('views', './src/views');
+
+
+/* ROTAS */
+
+// UTILIZANDO ROTAS DO ARQUIVO web.js
+app.use(routes)
+
+/* ESCUTA REQUISIÇÃO HTTP */
+
+
+app.listen(port, () => {
+  console.log(`Rodando em http://localhost:${port}`)
+})
---
+++ src/routes/web.js
@@ -0,0 +1,22 @@
+/* IMPORTA O MODULO ROUTER DO EXPRESS */
+const router = require('express').Router()
+
+/* ROTAS */
+
+// GET   /books             =>   listar todos livros
+// GET   /books/new         =>   exibir o form
+// POST  /books/new         =>   cadastrar o livro
+// GET   /books/:id         =>   exibir o livro
+// POST  /books/edit/:id    =>   editar o livro
+// POST  /books/delete/:id  =>   excluir o livro
+
+router.get('/', (req, res) => {
+    res.render('pages/home')
+  })
+
+router.get('/books/new', (req, res) => {
+    res.render('pages/book_form')
+  })
+
+/* EXPORTA AS ROTAS PARA OUTRO ARQUIVO */
+module.exports = router
